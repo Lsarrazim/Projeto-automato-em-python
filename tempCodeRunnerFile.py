cf5746d6d281835ac16403eaacc4565c1f3eaddc
Lucas Sarrazim <132080263+Lsarrazim@users.noreply.github.com>
2026-03-03 18:56:36 -0300

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+import pyautogui
+import pandas as pd
+import time
+import os
+
+
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
